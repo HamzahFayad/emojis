11f7bedfd934435e89dcbf0393c7a3c75f754757
Hamzah Fayad <hamzahfayad99@gmail.com>
2023-02-15 15:30:16 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import emojis from "./emojis.json";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>EmojiLand</h1>
+        <input value={search} onChange={handleSearch} placeholder="Search" />
+        <div className="emojis-list">
+          {emojis
+            .filter(
+              (emoji) =>
+                emoji.category.toLowerCase().includes(search.toLowerCase()) ||
+                emoji.description.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((emojiFiltered) => {
+              return (
+                <div className="emojis-item" key={emojiFiltered.description}>
+                  <p>{emojiFiltered.emoji}</p>
+                </div>
+              );
+            })}
+        </div>
+      </section>
     </div>
   );
 }
